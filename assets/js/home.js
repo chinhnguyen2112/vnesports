a5bb61ed8c050da165d1af1a52c636323f452d04
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-04-12 11:56:12 +0700

    update code lan 3

--- assets/js/home.js
+++ assets/js/home.js
@@ -14,7 +14,8 @@ $(".load_more").click(function () {
 			if (data.status == 0) {
 				$(".load_more").remove();
 			} else if (data.status == 1) {
-				$(".list_blog_home").append(data.html);
+				// $(".list_blog_home").append(data.html);
+				$(".load_more").before(data.html);
 				if (data.next == 0) {
 					$(".load_more").remove();
 				}
@@ -25,42 +26,7 @@ $(".load_more").click(function () {
 		},
 	});
 });
-var check = 1;
-$(window).scroll(function () {
-	if ($(".load_more").length) {
-		if ($(this).scrollTop() > $(".right_home").height() - 500) {
-			if (check == 1) {
-				console.log(1);
-				check = 0;
-				var form_data = new FormData();
-				form_data.append("page", page);
-				$.ajax({
-					url: "/load_more",
-					type: "POST",
-					processData: false,
-					contentType: false,
-					dataType: "json",
-					data: form_data,
-					success: function (data) {
-						++page;
-						if (data.status == 0) {
-							$(".load_more").remove();
-						} else if (data.status == 1) {
-							$(".list_blog_home").append(data.html);
-							if (data.next == 0) {
-								$(".load_more").remove();
-							}
-						}
-						check = 1;
-					},
-					error: function () {
-						alert("error");
-					},
-				});
-			}
-		}
-	}
-});
+
 $(".slick_banner").slick({
 	slidesToShow: 1,
 	arrows: false,
