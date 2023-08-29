12445883a3ba24a417aefc049c431427c49f9626
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-08-29 08:11:56 +0700

    Khanh update code ngay 29/08/2023 lan 1

--- application/helpers/func_helper.php
+++ application/helpers/func_helper.php
@@ -152,3 +152,33 @@ function remove_accent($mystring)
 
 	return str_replace($marTViet, $marKoDau, $mystring);
 }
+
+function replace_date($time)
+{
+	$weekday = date("l", $time);
+	$weekday = strtolower($weekday);
+	switch ($weekday) {
+		case 'monday':
+			$weekday = 'Thứ hai';
+			break;
+		case 'tuesday':
+			$weekday = 'Thứ ba';
+			break;
+		case 'wednesday':
+			$weekday = 'Thứ tư';
+			break;
+		case 'thursday':
+			$weekday = 'Thứ năm';
+			break;
+		case 'friday':
+			$weekday = 'Thứ sáu';
+			break;
+		case 'saturday':
+			$weekday = 'Thứ bảy';
+			break;
+		default:
+			$weekday = 'Chủ nhật';
+			break;
+	}
+	return $weekday . ', ' . date('d/m/Y - H:i ', $time);
+}
